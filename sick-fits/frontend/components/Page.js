79a81f5c7d47f31662669273b73c00414d4a880a
Mateusz Wojtas <nookdawn@gmail.com>
2022-01-17 22:43:35 +0100

    Create Nav and Header

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,11 @@
-export default function Page() {
+import Header from './Header';
+
+export default function Page({ children }) {
   return (
     <div>
+      <Header />
       <h2>I am the page component</h2>
+      {children}
     </div>
   );
 }
